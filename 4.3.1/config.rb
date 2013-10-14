330c09f63a5f38c50b98b7fb96bbcc8ba092ae55
Andrew Pietsch <andrew@arpsoft.com.au>
2013-10-14 18:08:00 +1100

    initial commit

---
+++ 4.3.1/config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+# require 'zurb-foundation'
+#require 'magick'
+
+# Set this to the root of your project when deployed:
+
+http_path = "/packages/arpsoft_theme_three/themes/arpsoft_theme_three"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+extensions_dir = "extensions"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+#output_style = :compressed
+output_style = :compact
---
+++ 4.3.2/js/compile.sh
@@ -0,0 +1,2 @@
+sprockets -I. foundation.js > foundation.max.js
+sprockets -I. --js-compressor=closure foundation.js > foundation.min.js
